bfc9a5d9c1b8f15f6dfd63364fa169bfc0df59c7
Mike Shelby <mikegshelby@gmail.com>
2020-10-17 11:24:05 -0500

    add styles to task list under current dragged element

--- assets/js/script.js
+++ assets/js/script.js
@@ -206,6 +206,7 @@ var dropZoneDragHandler = function(event) {
   var taskListEl = event.target.closest(".task-list");
   if (taskListEl) {
     event.preventDefault();
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed; box-shadow: none;");
   }
 };
 
@@ -230,7 +231,16 @@ var dropTaskHandler = function(event) {
   }
 
   dropZoneEl.appendChild(draggableElement);
+
+  dropZoneEl.removeAttribute("style");
 };
+
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    taskListEl.removeAttribute("style");
+  }
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
@@ -241,3 +251,5 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
